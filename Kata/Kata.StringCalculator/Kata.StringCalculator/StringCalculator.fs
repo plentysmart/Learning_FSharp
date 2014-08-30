197254878940538a491ac91ca155b2c38bfd9505
plentysmart <github@plentysmart.co.uk>
2014-08-30 19:53:04 +0100

    StringCalculator Kata part 1

---
+++ Kata/Kata.StringCalculator/Kata.StringCalculator/StringCalculator.fs
@@ -0,0 +1,35 @@
+﻿namespace Kata
+open System
+type StringCalculator() = 
+    let parseNumber (numberString:string) = 
+        Int32.Parse(numberString)
+    let defaultDelimiters = [|',';'\n'|]
+    let extractDelimiters (stringExpression:string)= 
+            let charListExpression =List.ofArray (stringExpression.ToCharArray())
+            match charListExpression with
+            |'/'::'/'::d::'\n'::tail -> 
+                let rec skipUntilNewLine (stringList:char list)= 
+                        match stringList with
+                        | [] -> []
+                        | '\n'::tail -> tail
+                        | _::tail -> skipUntilNewLine tail
+                let expressionOnlyList = skipUntilNewLine charListExpression
+                [|d|],String(Array.ofList expressionOnlyList)
+            |_ -> defaultDelimiters,stringExpression
+    member x.Add(numbersExpression:string):int =
+        let delimiters, expression = extractDelimiters numbersExpression
+        let stringNumbers = List.ofArray (expression.Split (delimiters,StringSplitOptions.RemoveEmptyEntries ))
+        if List.isEmpty stringNumbers then 
+            0
+        else
+           let numbers = 
+                stringNumbers
+                    |> List.map parseNumber
+           if List.exists (fun x -> x < 0) numbers then
+                let negativeNumbers = numbers |> List.filter (fun x-> x< 0)
+                let negativeArguments = String.Join("\n", negativeNumbers)
+                let message = "negatives not allowed\n"+negativeArguments
+                raise (ArgumentException(message))
+            else
+                numbers
+                |> List.reduce (+)
---
+++ Kata/Kata.StringCalculator/Kata.StringCalculator/Tests.fs
@@ -0,0 +1,41 @@
+﻿module Tests
+open NUnit.Framework
+open Kata
+open System
+[<TestFixture>]
+type Add() =
+    
+    let calculator = StringCalculator()
+    let verifyCalculator (parameter:string) (expected:int) = 
+        let actual = calculator.Add parameter
+        Assert.AreEqual(expected,actual)
+    
+    [<Test>]
+    member x.``called with zero numbers (empty string) should return 0``() =
+        verifyCalculator "" 0
+    [<Test>]
+    member x.``called with one number should returns this number``() =
+        verifyCalculator "1" 1
+    [<Test>]
+    member x.``called with two numbers should return their sum``() =
+        verifyCalculator "1,2" 3
+    [<Test>]
+    member x.``called with any number of number should return their sum``() =
+        verifyCalculator "1,2,3,4,5,6,7,8,10,0,10" 56
+    [<Test>]
+    member x.``should handle \n and , as delimiter``() =
+        verifyCalculator "1\n2,3" 6
+    [<Test>]
+    member x.``//[delimiter]\n at the beggining of the expression should set new delimiter``() =
+        verifyCalculator "//;\n1;2" 3
+    [<Test>]
+    member x.``called with negative number should throw exception "negatives not allowed"``() =
+        let exceptionThrown =
+            try
+                let result = calculator.Add "-3"
+                false
+            with
+                | :? ArgumentException as x -> 
+                    Assert.AreEqual("negatives not allowed\n-3",x.Message)
+                    true
+        Assert.IsTrue(exceptionThrown)
